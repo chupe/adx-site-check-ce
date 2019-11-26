61bbf1433e5145b2e6545e9a71214611ddeb019b
chupe <>
2019-11-26 23:49:55 +0100

    Added comments for clarification

--- content.js
+++ content.js
@@ -5,19 +5,23 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
     let adUnitsInfo = data
 
     let adUnits = $('div[id^="div-gpt-ad-"]')
-
+    // Fill adunit.sizes. Regexp to match the sizes array, than to match
+    // individual size pair, parsed as ints and sorted according to surface area.
     let extractSizes = (name) => {
         let scripts = document.scripts
+
+        // Prepare adunit.name to be used inside regexp obj
         name = name.replace('.', '\\.')
         let regex = new RegExp("(?<=/" + name + "', ?\\[ ?).+(?= ?\\] ?, ?)", 'g')
         let sizes = ''
         for (let script of scripts) {
+            // If there is a script and sizes array hasnt been populated yet
             if (script && !sizes) {
                 sizes = script.textContent.match(regex)
                 if (sizes) {
                     let reg = new RegExp(", ?", 'g')
-                    // If ad unit has one size it does not have braces,
-                    //and requires different approach to turn it into array of int
+                    // If ad unit has one size it does not have two pairs of braces,
+                    // and requires different approach to turn it into array of int
                     if (!sizes[0].match(/\[[0-9]{3},[ ]*[0-9]{2,}\]/g)) {
                         sizes = sizes[0].split(reg)
                         sizes = [parseInt(sizes[0]), parseInt(sizes[1])]
@@ -32,10 +36,11 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
 
                             return size
                         })
+
+                        // Sort according to surface area of adunit
                         sizes.sort((a, b) => {
-                            return a[0] * a[1] - b[0] * b[1]
+                            return b[0] * b[1] - a[0] * a[1]
                         })
-                        sizes.reverse()
                     }
                 }
             }
@@ -44,6 +49,7 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
         return sizes
     }
 
+    // Converts sizes array to a string in a readable format
     let sizesAsText = (adUnit) => {
         let sizesAsText = ''
         for (let size of adUnit.sizes) {
@@ -54,13 +60,7 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
         return sizesAsText.substring(0, sizesAsText.length - 2)
     }
 
-    let refreshAdUnitsInfo = () => {
-        chrome.storage.sync.get(originUrl.hostname, (data) => {
-            adUnitsInfo = data
-        })
-    }
-
-
+    // Add css to make adunits visible even when they are unfilled
     let highlightAdUnits = function () {
         adUnits.css("display", "")
         adUnits.removeClass('hideAdUnits')
@@ -74,9 +74,15 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
                 let height = 0
                 let width = 0
 
+                // The adunit is susposed to contain the name and all
+                // sizes as innerHTML
                 for (let adUnit in adUnitsInfo[originUrl.hostname]) {
                     let elementId = element.getAttribute('id')
                     let currentAdUnit = adUnitsInfo[originUrl.hostname][adUnit]
+
+                    // Since div contains only ID and adUnitsInfo object
+                    // has names as keys the adUnitsInfo object need to be
+                    // matched by ID and accessed by adunit name
                     if (elementId == currentAdUnit.ID) {
                         let sizes = sizesAsText(currentAdUnit)
                         name = currentAdUnit.name
@@ -93,15 +99,19 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
         })
     }
 
+    // Remove css that makes adunits highlighted
     let unhighlightAdUnits = function () {
         adUnits.removeClass('highlightAdUnits')
-        adUnits.text('')
     }
 
+    // Hide all adunits
     let hideAdUnits = function () {
         adUnits.addClass('hideAdUnits')
     }
 
+    // Callback to adunits.onclick,
+    // changes size of highlighted adunit in descending order 
+    // of adunit.sizes array
     let changeSize = function () {
         let sizes = adUnitsInfo[originUrl.hostname][$(this).attr('title')].sizes
         let width = $(this).width()
@@ -115,6 +125,8 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
         }
     }
 
+    // Append custom CSS and listeners. Check before appending if it has been
+    // appended before
     let addListeners = function () {
         if (!cssAppended) {
             $(`<style>
@@ -134,9 +146,13 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
     }
 
     let removeListeners = function () {
-        unhighlightAdUnits();
+        unhighlightAdUnits()
     }
 
+    // Find adxbid script inside the script tags by iterating them
+    // with each and searching a string. If found sendMessage for popup.js
+    // to download the script, else call checkTags with '' so it skips
+    // looking for tags inside script
     let extractScript = () => {
         let scriptUrl = ''
         $("script").each((_index, element) => {
@@ -145,7 +161,7 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
                 scriptUrl = src
             }
         })
-        console.log(scriptUrl)
+
         if (scriptUrl) {
             chrome.runtime.sendMessage({ command: 'scriptUrl', url: scriptUrl }, (response) => {
                 if (response)
@@ -155,8 +171,12 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
         } else checkTags('')
     }
 
+    // Contains three functions to get information in the form of an array
+    // from the script, head and body sections. After it has been completed
+    // the resulting arrays go inside evaluateTags.
     let checkTags = (script) => {
         // Returns an array of div-gpt tags from adxbid script
+        // unless parameter is missing. If so returns empty array
         let tagsFromScript = (scriptBody) => {
             let scriptTags = ''
             if (scriptBody)
@@ -180,6 +200,8 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
         let tagsFromHead = () => {
             let headTags = []
 
+            // Iterate over all scripts in the head and match id
+            // and name
             for (let script of document.scripts) {
                 if (script && headTags.length == 0) {
                     adUnitIDs = script.textContent.match(/div-gpt-ad-[0-9]{13}-\d/g)
@@ -187,6 +209,10 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
 
                     if (adUnitIDs) {
                         for (let i = 0; i < adUnitIDs.length; i++) {
+                            // In rare cases when sizes array contains 'fluid'
+                            // the name isnt matched correctly. When this is the case
+                            // the name is substringed up to the first occurence of - ', marking
+                            // the end of actual adunit name
                             if (adUnitNames[i].search("',") > -1)
                                 adUnitNames[i] = adUnitNames[i].substring(0, adUnitNames[i].search("',"))
                             headTags.push({
@@ -202,7 +228,9 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
             return headTags
         }
 
-        //Returns an array of objects containing info about each tag
+        // Returns an array of objects containing info about each tag.
+        // Object keys are named by adunit names. The function iterates over
+        // headTags and compares with body and script arrays
         let evaluateTags = (headTags, bodyDivs, scriptTags) => {
             let adUnitsInfo = {}
             for (let headTag of headTags) {
@@ -222,9 +250,10 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
                         adUnit.inArticleBody = true
                 }
 
-                // This is a signal that the information inside the Adunit object
-                // has not been checked in this function call. It depends if current page
-                // is homepage or article
+                // If the current location is homepage than inArticle is marked
+                // as 'unchecked' so as not to produce false errors. 'unchecked'
+                // field values get replaced accordingly when the object is updated
+                // inside popup.js
                 if (originUrl.pathname == '/')
                     adUnit.inArticleBody = 'unchecked'
                 if (originUrl.pathname != '/')
@@ -247,6 +276,7 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
                 this.inArticleBody = false
                 this.inScript = false
                 this.sizes = []
+                this.missingAdsTxtLines = []
             }
             static count = 1
         }
@@ -256,6 +286,7 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
         let scriptTags = tagsFromScript(script)
         let adUnitsInfo = evaluateTags(headTags, bodyDivs, scriptTags)
 
+        // Upon completion the adUnitsInfo object is passed to the popup.js
         chrome.runtime.sendMessage({ command: 'adUnitsInfo', adUnitsInfo: adUnitsInfo })
     }
 
@@ -284,7 +315,7 @@ chrome.storage.sync.get(originUrl.hostname, (data) => {
     //on init perform based on chrome storage value
     window.onload = function () {
         chrome.storage.sync.get('highlight', function (data) {
-            if (data.hide) {
+            if (data.highlight) {
                 addListeners()
             } else {
                 removeListeners()
--- popup.js
+++ popup.js
@@ -4,9 +4,16 @@ let checkTags = document.getElementById('checkTags')
 let showDetails = document.getElementById('showDetails')
 let checkAdsTxt = document.getElementById('checkAdsTxt')
 let clearStorage = document.getElementById('clearStorage')
-let bkg = chrome.extension.getBackgroundPage()
 let errsList = document.getElementById("errsList")
 let adUnitErrs = document.getElementById('adUnitErrs')
+
+// bkg provides acces to extension console since popup.js and popup.html have console separate
+// from content and extension
+let bkg = chrome.extension.getBackgroundPage()
+
+// Get activeTabUrl to check against it in different parts of the file
+// and to be able to fetch fresh information from storage
+// that is relevant to the currently active tab url
 let activeTabUrl = chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     activeTabUrl = new URL(tabs[0].url)
     chrome.storage.sync.get(activeTabUrl.hostname, function (data) {
@@ -20,17 +27,18 @@ clearStorage.onclick = () => {
     })
 }
 
+// Get saved value each time the extension icon is clicked
 chrome.storage.sync.get('highlight', (data) => {
-    highlightAdUnits.value = data.highlight
+    highlightAdUnits.checked = data.highlight
 })
 
-highlightAdUnits.onchange = function (element) {
+highlightAdUnits.onchange = function () {
     let value = this.checked
 
-    //update the extension storage value
+    // Update the extension storage value
     chrome.storage.sync.set({ 'highlight': value })
 
-    //Pass init or remove message to content script 
+    // Pass highlight or unhighlight message to content script 
     if (value) {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { command: "highlight", highlight: value }, function (response) {
@@ -44,8 +52,7 @@ highlightAdUnits.onchange = function (element) {
             })
         })
     }
-
-};
+}
 
 hideAdUnits.onclick = function (element) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -55,16 +62,60 @@ hideAdUnits.onclick = function (element) {
     })
 }
 
+// Pass to the function the information from the content script and the origin url so it can be
+// saved in storage under publisher name key
+let updateStorage = (adUnitsUpdate, publisher) => {
+    chrome.storage.sync.get(publisher, (storedInfo) => {
+        let isStoredInfoEmpty = Object.keys(storedInfo).length === 0 && storedInfo.constructor === Object
+
+        // If the no information is stored for the publisher
+        // create a new entry named [publisher]
+        if (isStoredInfoEmpty) {
+            let json = new Object()
+            json[publisher] = adUnitsUpdate
+            chrome.storage.sync.set(json)
+        } else {
+
+            // Update the 'unchecked' fields
+            for (let adUnit in storedInfo[publisher]) {
+                let inHomeUnchecked = adUnitsUpdate[adUnit].inHomepageBody == 'unchecked'
+                let inArticleUnchecked = adUnitsUpdate[adUnit].inArticleBody == 'unchecked'
+
+                if (!inHomeUnchecked)
+                    storedInfo[publisher][adUnit].inHomepageBody = adUnitsUpdate[adUnit].inHomepageBody
+
+                if (!inArticleUnchecked)
+                    storedInfo[publisher][adUnit].inArticleBody = adUnitsUpdate[adUnit].inArticleBody
+
+            }
+
+            chrome.storage.sync.set(storedInfo)
+        }
+    })
+}
+
 checkAdsTxt.onclick = function () {
     let localAdstxt = ''
     let adstxt = ''
     let missingLines = []
 
+    // The function loads static ads.txt from local folder, splits it into
+    // an array at '\n' than splits it once more so that each comma in a line
+    // is a split point. This way each comma separated value in each line can
+    // be compared against another line that is generated in the same way but
+    // originates from the activeTabUrl.origin /ads.txt
     let compareAdsTxt = () => {
+
+        // Split lines first
         localAdstxt = localAdstxt.split('\n')
         adstxt = adstxt.split('\n')
+
         for (let localLine of localAdstxt) {
             let onSite = false
+            // Each line is split into comma separated list (bidder name, publisher ID)
+            // trimmed of spaces for consistency. Iterate over local ads.txt and try to
+            // match it against the origin site ads.txt. If unable push missing lines
+            // into a variable missingLines
             let localItems = localLine.split(',').map((item) => item.trim())
             for (let siteLine of adstxt) {
                 let siteItems = siteLine.split(',').map((item) => item.trim())
@@ -75,6 +126,8 @@ checkAdsTxt.onclick = function () {
             if (!onSite)
                 missingLines.push(localLine)
         }
+
+        // So far the value is only shown inside the extension console
         bkg.console.log(missingLines)
     }
 
@@ -88,12 +141,16 @@ checkAdsTxt.onclick = function () {
         compareAdsTxt()
     }
 
+    // Prepare local XHR, inside its request listener call the send()
+    // for origin ads.txt.
     let local = new XMLHttpRequest()
     local.open("GET", './ads.txt')
     local.responseType = 'text'
     local.addEventListener("loadend", reqListenerLocal)
     local.send()
 
+    // Remote send() is called from reqListenerLocal to make sure that
+    // both adstxt and localAdsTxt are resolved before calling compare function
     let hostAdsTxt = activeTabUrl.origin + '/ads.txt'
     let onsite = new XMLHttpRequest()
     onsite.open("GET", hostAdsTxt)
@@ -104,18 +161,22 @@ checkAdsTxt.onclick = function () {
 
 showDetails.onclick = function () {
     chrome.storage.sync.get(activeTabUrl.hostname, (data) => {
+
+        // The detailed information about the current site is displayed
+        // inside extension console
         bkg.console.log(data)
     })
 }
 
+// Check tags function sendsMessage for the content script to check tags
 checkTags.onclick = function (element) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { command: "checkTags" }, function (response) {
-            bkg.console.log(response.result)
-        })
+        chrome.tabs.sendMessage(tabs[0].id, { command: "checkTags" })
     })
 }
 
+// Function to remove HTML nodes completly since they only get hidden
+// and child elements are stacking upon each apppendChild call
 let removeNode = (node) => {
     while (node.lastChild) {
         node.removeChild(node.lastChild);
@@ -123,14 +184,23 @@ let removeNode = (node) => {
     node.remove()
 }
 
+// Update displayed info. When changes object is passed the function check
+// if there are 'unchecked' keys, in which case the user should check them as
+// suggested in the help text. The function appends the names of all adunits
+// and displays brief error information
 let updateInfo = (changes) => {
     removeNode(adUnitErrs)
     if (changes) {
         for (let adUnit in changes) {
+            // Create parent element per adunit
             let err = document.createElement('ul')
+            // Create children per adunit errs
             let msg = document.createElement('li')
             err.innerText = changes[adUnit].name
 
+            // If else checks if inHomepage and inArticle information is
+            // available. If not it suggests to the user to make the missing
+            // check.
             if (changes[adUnit].inArticleBody === 'unchecked') {
                 msg.innerText = 'Now check Article'
                 err.appendChild(msg)
@@ -139,12 +209,14 @@ let updateInfo = (changes) => {
                 msg.innerText = 'Now check homepage'
                 err.appendChild(msg)
             } else {
-
+                // If both values, inHomepageBody and inArticleBody are false the error
+                // message states it
                 if (!changes[adUnit].inHomepageBody && !changes[adUnit].inArticleBody) {
                     msg.innerText = 'NOT found in body'
                     err.appendChild(msg)
                 }
 
+                // If inScript value is false it is shown in the popup display
                 if (!changes[adUnit].inScript) {
                     msg.innerText = 'NOT found in script'
                     err.appendChild(msg)
@@ -163,6 +235,11 @@ chrome.storage.onChanged.addListener((changes) => {
         if (changes[activeTabUrl.hostname]) {
             let isNew = changes[activeTabUrl.hostname].newValue
 
+            // If there are new values regarding the current active tab
+            // they are passed on to the updateInfo function so the displayed
+            // information can be updated. Else the changes obj contains only
+            // oldInfo key meaning the storage has been cleared (newValue == undefined)
+            // requiring the popup.html to be cleared of all error info elements
             if (isNew)
                 updateInfo(changes[activeTabUrl.hostname].newValue)
             else
@@ -170,74 +247,43 @@ chrome.storage.onChanged.addListener((changes) => {
         }
     }
     )
+})
+
+// Script url is the url extracted in content, passed here in a message and the script body
+// will be sent back to content as a response to that message.
+let fetchScript = (scriptUrl, sendResponse) => {
+    function reqListener() {
+        let response = this.responseText
+        if (response)
+            sendResponse(response)
+        else sendResponse('')
+    };
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', scriptUrl, true)
+    xhr.responseType = 'text'
+    xhr.addEventListener("loadend", reqListener)
+    xhr.send()
 }
-)
 
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-
-        let fetchScript = () => {
-            function reqListener() {
-                let response = this.responseText
-                if (response)
-                    sendResponse(response)
-                else sendResponse('')
-            };
-
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', request.url, true)
-            xhr.responseType = 'text'
-            xhr.addEventListener("loadend", reqListener)
-            xhr.send()
-        }
-
-        let updateStorage = () => {
-
-            let adUnitsUpdate = request.adUnitsInfo
-            let publisher = new URL(sender.tab.url).hostname
-
-            chrome.storage.sync.get(publisher, (storedInfo) => {
-
-                let isStoredInfoEmpty = Object.keys(storedInfo).length === 0 && storedInfo.constructor === Object
-
-                // If the no information is stored for the publisher
-                // create a new entry named [publisher]
-                if (isStoredInfoEmpty) {
-                    let json = new Object()
-                    json[publisher] = adUnitsUpdate
-                    chrome.storage.sync.set(json)
-                } else {
-
-                    // Update the 'unchecked' fields
-                    for (let adUnit in storedInfo[publisher]) {
-                        let inHomeUnchecked = adUnitsUpdate[adUnit].inHomepageBody == 'unchecked'
-                        let inArticleUnchecked = adUnitsUpdate[adUnit].inArticleBody == 'unchecked'
-
-                        if (!inHomeUnchecked)
-                            if (storedInfo[publisher][adUnit].inHomepageBody != adUnitsUpdate[adUnit].inHomepageBody)
-                                storedInfo[publisher][adUnit].inHomepageBody = adUnitsUpdate[adUnit].inHomepageBody
-
-                        if (!inArticleUnchecked)
-                            if (storedInfo[publisher][adUnit].inArticleBody != adUnitsUpdate[adUnit].inArticleBody)
-                                storedInfo[publisher][adUnit].inArticleBody = adUnitsUpdate[adUnit].inArticleBody
-
-                    }
-
-                    chrome.storage.sync.set(storedInfo)
-                }
-            })
-        }
+        let publisher = new URL(sender.tab.url).hostname
 
         switch (request.command) {
             case 'scriptUrl':
-                fetchScript()
+                fetchScript(request.url, sendResponse)
                 break
             case 'adUnitsInfo':
-                updateStorage()
+                updateStorage(request.adUnitsInfo, publisher)
                 sendResponse()
                 break
+            default:
+                sendResponse({ result: "Unrecognized request.command" })
         }
 
+        // True needs to be returned in all message listeners in order to keep the message channel
+        // open until a reponse is received
         return true
     }
 )
